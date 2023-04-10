e212f4b6310e9b7072b3ad9fe5d46f4627461213
T.Shinohara <tshinoharawork@gmail.com>
2023-04-10 21:38:48 +0900

    初回コミット

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:16-buster-slim
+
+RUN apt-get update && \
+    apt-get -y install locales && \
+    localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
+ENV LANG ja_JP.UTF-8
+ENV LANGUAGE ja_JP:ja
+ENV LC_ALL ja_JP.UTF-8
+ENV TZ JST-9
+ENV TERM xterm
+ENV HOST 0.0.0.0
+
+RUN apt-get update && \
+    apt-get install -y vim less
+
+RUN apt-get install -y curl openjdk-11-jre-headless
+
+RUN npm install -g firebase-tools
+
+WORKDIR /app
---
+++ app/index.js
@@ -0,0 +1,38 @@
+import bsky from '@atproto/api'
+import moment from "moment"
+
+const { BskyAgent } = bsky
+
+let self = null
+const agent = new BskyAgent({ service: 'https://bsky.social' })
+
+const login = async function () {
+    try {
+        const { success, data } = await agent.login({
+            identifier: process.env.AUTHOR,
+            password: process.env.PASSWORD,
+        })
+        self = data;
+        return success ? data : null
+    } catch {
+        return null
+    }
+}
+
+const post = async function (text) {
+    return agent.api.app.bsky.feed.post.create(
+        { repo: self.handle },
+        {
+            text: text,
+            createdAt: new Date().toISOString(),
+        }
+    )
+}
+
+const result = await login();
+if (result) {
+    let time = moment().format('YYYY/MM/DD HH:mm:ss');
+    console.log(self.handle);
+    console.log("なう(" + time + ")");
+    // post("なう(" + time + ")");
+}
