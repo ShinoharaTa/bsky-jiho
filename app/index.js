784518630419694722fcc16abed4f770ddbe43cc
tshinohara <takuma1201@gmail.com>
2023-04-16 23:36:51 +0900

    実行条件を修正

--- app/index.js
+++ app/index.js
@@ -1,36 +1,38 @@
-import bsky from '@atproto/api'
-import moment from "moment-timezone"
+import dotenv from 'dotenv';
+import bsky from "@atproto/api";
+import moment from "moment-timezone";
 
-const { BskyAgent } = bsky
+dotenv.config();
+const { BskyAgent } = bsky;
 
-let self = null
-const agent = new BskyAgent({ service: 'https://bsky.social' })
+let self = null;
+const agent = new BskyAgent({ service: "https://bsky.social" });
 
 const login = async function () {
-    try {
-        const { success, data } = await agent.login({
-            identifier: process.env.AUTHOR,
-            password: process.env.PASSWORD,
-        })
-        self = data;
-        return success ? data : null
-    } catch {
-        return null
-    }
-}
+  try {
+    const { success, data } = await agent.login({
+      identifier: process.env.AUTHOR,
+      password: process.env.PASSWORD,
+    });
+    self = data;
+    return success ? data : null;
+  } catch {
+    return null;
+  }
+};
 
 const post = async function (text) {
-    return agent.api.app.bsky.feed.post.create(
-        { repo: self.handle },
-        {
-            text: text,
-            createdAt: new Date().toISOString(),
-        }
-    )
-}
+  return agent.api.app.bsky.feed.post.create(
+    { repo: self.handle },
+    {
+      text: text,
+      createdAt: new Date().toISOString(),
+    }
+  );
+};
 
 const result = await login();
 if (result) {
-    const time = moment().tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm:ss');
-    post("なう(" + time + ")");
+  const time = moment().tz("Asia/Tokyo").format("YYYY/MM/DD HH:mm:ss");
+  post("なう(" + time + ")");
 }
